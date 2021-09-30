e03e317791eb6b08aff423db2f23bc1c23a93eaa
gabriele.sipione@hotmail.com <jager@DESKTOP-PQFGES5.localdomain>
2021-09-30 17:54:50 +0200

    Lab1 commit!

---
+++ Lab1/lab1.py
@@ -0,0 +1,25 @@
+#calculator.py
+
+def sum(m,n):
+    if type(n) is int :
+        for i in range(abs(n)):
+            if n > 0 : 
+                m += 1
+            else :
+                m -= 1
+        return m
+    raise ValueError("It works only in Z")
+
+def divide(m,n):
+    absM = abs(m)
+    absN = abs(n)
+    i = 0
+    if type(n) is int and n != 0 :
+        while absM >= absN:
+                absM -= absN
+                i +=1  
+        if (n > 0 and m > 0) or (n < 0 and m < 0):
+            return i
+        else :
+            return -i
+    raise ValueError("It only works with positives != 0")
---
+++ Lab1/test.py
@@ -0,0 +1,5 @@
+from fooCalculator import FooCalculator as foo
+
+calc = foo()
+
+print(calc.sum(2,5))https://github.com/GabrieleSipio/AdvancedSoftwareEngeneering.git
